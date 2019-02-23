4780f69893fa09114a96616584f05eb92f9c73ba
Esther Weyinmi Isack <your1cent@gmail.com>
2019-02-23 02:50:33 +0100

    First text added to Frame with extension to JFrame class.

--- SwDemo/src/swdemo/SecondGUI.java
+++ SwDemo/src/swdemo/SecondGUI.java
@@ -6,14 +6,31 @@
 package swdemo;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
  * @author koloh
  */
 public class SecondGUI {
+    public static void main(String[] args) {
+        Abcs obj = new Abcs();
+        
+    }
     
 }
 class Abcs extends JFrame{
+    
+    // Create a constructor
+    public Abcs(){
+        
+        JLabel jl = new JLabel("Hello World");
+        add(jl);
+        
+        // Always better to write your properties inside constructor
+        setVisible(true);
+        setSize(400, 400);
+        
+    }
     
 }
