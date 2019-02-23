952fc995bbd7444296c95132d3b23d66e4e8fac7
Esther Weyinmi Isack <your1cent@gmail.com>
2019-02-23 02:24:44 +0100

    GUI class created.

---
+++ SwDemo/src/swdemo/SecondGUI.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package swdemo;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author koloh
+ */
+public class SecondGUI {
+    
+}
+class Abcs extends JFrame{
+    
+}
